ef6cf14ea6954a68263a782c6b9bb77b437d434b
Nebele <120013224+Nebele@users.noreply.github.com>
2026-04-29 19:20:21 +0300

    Create landing page for Алгоритмика Пенза school website

---
+++ script.js
@@ -0,0 +1,7 @@
+const demoBtn = document.getElementById('demoBtn');
+
+if (demoBtn) {
+  demoBtn.addEventListener('click', () => {
+    alert('Спасибо! Мы свяжемся с вами в ближайшее время для записи на пробный урок.');
+  });
+}
